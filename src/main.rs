049fd1600047c4f266324f8c90865afc68934362
John Peel <john@dgby.org>
2020-06-05 00:57:16 -0400

    Moved DB to global variable using lazy_static

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate lazy_static;
+
+use std::sync::Mutex;
+use std::collections::HashMap;
 
 use rocket::response::Redirect;
 use pickledb::{ PickleDb, PickleDbDumpPolicy, SerializationMethod };
-use std::collections::HashMap;
 
-fn get_value(to: &str, from: &str) -> u32 {
-    let db = PickleDb::load("data.json",
-                            PickleDbDumpPolicy::AutoDump,
-                            SerializationMethod::Json).unwrap();
-    let entry = db.get::<HashMap<String, u32>>(&from)
-        .unwrap_or_else(|| HashMap::new());
-    *entry.get(to).unwrap_or_else(|| &0u32)
+lazy_static! {
+    pub static ref DB: Mutex<PickleDb> = Mutex::new(PickleDb::load("data.json",
+            PickleDbDumpPolicy::AutoDump,
+            SerializationMethod::Json)
+        .unwrap_or_else(|_| PickleDb::new("data.json",
+            PickleDbDumpPolicy::AutoDump,
+            SerializationMethod::Json)));
 }
 
-fn set_value(to: &str, from: &str, value: u32) {
-    let mut db = PickleDb::load("data.json",
-                                PickleDbDumpPolicy::AutoDump,
-                                SerializationMethod::Json).unwrap();
-
-    let mut map = db.get::<HashMap<String, u32>>(&from)
+fn get_value(to: &str, from: &str) -> u32 {
+    let map = DB.lock().unwrap().get::<HashMap<String, u32>>(&from)
         .unwrap_or_else(|| HashMap::new());
 
-    map.insert(to.parse().unwrap(), value);
-    db.set::<HashMap<String, u32>>(&from, &map).unwrap();
+    *map.get(to).unwrap_or_else(|| &0u32)
+}
+
+fn inc_value(to: &str, from: &str) {
+    let mut map = DB.lock().unwrap().get::<HashMap<String, u32>>(&from)
+        .unwrap_or_else(|| HashMap::new());
+
+    map.insert(to.to_string(), *map.get(to).unwrap_or_else(|| &0u32) + 1);
+    DB.lock().unwrap().set::<HashMap<String, u32>>(&from, &map).unwrap();
 }
 
 #[get("/stat/<to>/<from>")]
@@ -34,17 +40,10 @@ fn stat(to: String, from: String) -> String {
 
 #[get("/<to>/<from>")]
 fn redirect(to: String, from: String) -> Redirect {
-    set_value(&to, &from, get_value(&to, &from) + 1);
+    inc_value(&to, &from);
     Redirect::permanent("https://www.youtube.com/watch?v=d1YBv2mWll0")
 }
 
 fn main() {
-    if let Err(_) = PickleDb::load("data.json",
-                                   PickleDbDumpPolicy::AutoDump,
-                                   SerializationMethod::Json) {
-        PickleDb::new("data.json",
-                      PickleDbDumpPolicy::AutoDump,
-                      SerializationMethod::Json);
-    }
     rocket::ignite().mount("/", routes![stat, redirect]).launch();
 }
